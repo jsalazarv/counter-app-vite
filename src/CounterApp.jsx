bc603aaa6012025fb5a00e80f29a43eebccacb6e
Juan Salazar <jsalazarv8@gmail.com>
2022-08-28 23:18:04 -0500

    Added counter functionality

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import propTypes from 'prop-types';
+
+
 export const CounterApp = ({ value }) => {
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = () => {
+        setCounter( counter + 1 );
+    };
+
+    const handleSubtract = () => {
+        setCounter( counter - 1 );
+    };
+
+    const handleReset = () => {
+       setCounter( value );
+    };
+
     return (
         <>
-            <h2>Counter App</h2>
+            <button onClick={ handleSubtract }>-1</button>
+            <div>{ counter }</div>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
         </>
     );
 };
+
+CounterApp.propTypes = {
+    value: propTypes.number.isRequired
+}
+
+CounterApp.defaultProps = {
+    value: null
+}
